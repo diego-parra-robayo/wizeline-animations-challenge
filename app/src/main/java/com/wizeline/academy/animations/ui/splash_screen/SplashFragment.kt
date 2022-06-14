9bbc22f8accc32432b45e023eceb1025358c3957
Diego Parra <diego.parra@wizeline.com>
2022-06-13 22:11:50 -0500

    Splash Screen: Animate logo using ObjectAnimator.

--- app/src/main/java/com/wizeline/academy/animations/ui/splash_screen/SplashFragment.kt
+++ app/src/main/java/com/wizeline/academy/animations/ui/splash_screen/SplashFragment.kt
@@ -1,9 +1,12 @@
 package com.wizeline.academy.animations.ui.splash_screen
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnticipateOvershootInterpolator
+import android.view.animation.LinearInterpolator
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
@@ -15,6 +18,25 @@ class SplashFragment : Fragment() {
 
     private var _binding: SplashFragmentBinding? = null
     private val binding get() = _binding!!
+    private lateinit var target: View
+
+    private val rotateAnimator by lazy {
+        ObjectAnimator.ofFloat(target, View.ROTATION, 0f, 360f).apply {
+            repeatCount = 2
+            repeatMode = ObjectAnimator.RESTART
+            duration = 2000
+            interpolator = AnticipateOvershootInterpolator()
+        }
+    }
+
+    private val blinkAnimator by lazy {
+        ObjectAnimator.ofFloat(target, View.ALPHA, 0f, 1f).apply {
+            repeatCount = 2
+            repeatMode = ObjectAnimator.REVERSE
+            duration = 750
+            interpolator = LinearInterpolator()
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,15 +44,24 @@ class SplashFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = SplashFragmentBinding.inflate(inflater, container, false)
+        target = binding.ivWizelineLogo
         return binding.root
     }
 
     override fun onResume() {
         super.onResume()
         lifecycleScope.launch {
+            runAnimation(rotateAnimator)
+            runAnimation(blinkAnimator)
             delay(2000)
             goToHomeScreen()
         }
+    }
+
+    private fun runAnimation(objectAnimator: ObjectAnimator) {
+        objectAnimator
+            .apply { target = this@SplashFragment.target }
+            .start()
     }
 
     private fun goToHomeScreen() {
